771fcf85ca2fa88750963345477c895623584779
S B <se.email.exe@gmail.com>
2022-09-17 13:33:17 -0600

    Remove LevelSequence.cs

--- Assets/Scripts/GamePlay/GoalArea.cs
+++ Assets/Scripts/GamePlay/GoalArea.cs
@@ -1,18 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GoalArea : MonoBehaviour
 {
-	[Tooltip("The level sequence to use when deciding which level to load next.")]
-	public LevelSequence levelSequence;
-
 	private void OnTriggerEnter2D(Collider2D other)
 	{
 		if(other.GetComponentInParent<Player>() != null)
 		{
 			// Win the level.
-			levelSequence.LoadNextLevel();
+			// Move to the next level.
+			int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+			if(currentSceneIndex < SceneManager.sceneCountInBuildSettings-1)
+				SceneManager.LoadScene(currentSceneIndex + 1);
 		}
 	}
 }
