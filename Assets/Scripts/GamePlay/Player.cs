f916d3b1709114889395347411e974905cab3aa9
S B <se.email.exe@gmail.com>
2022-09-16 20:09:16 -0600

    Add PlayerCommander.cs
    Add RollingPlayerCommander.cs
    Add OnlyPunchingPlayerCommander.cs

--- Assets/Scripts/GamePlay/Player.cs
+++ Assets/Scripts/GamePlay/Player.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+	public const int armCount = 5;
+
 	public void PunchWithArm(int armIndex)
 	{
 		Debug.Log("Punch "+armIndex);
@@ -12,13 +14,22 @@ public class Player : MonoBehaviour
 
 	public void ExtendArm(int armIndex, Vector3 targetPosition)
 	{
-		Debug.Log("Extend " + armIndex);
+		Debug.Log("Extend " + armIndex + ", " + targetPosition);
 		// TODO
 	}
 
 	public void DetachArm(int armIndex)
 	{
 		Debug.Log("Detach " + armIndex);
+		// TODO
+	}
+
+	/// <summary></summary>
+	/// <param name="torqueDirection"> From -1..1, where -1 and 1 apply maximum roll torque in opposite directions. </param>
+	public void ApplyRollTorque(float torqueDirection)
+	{
+		if(torqueDirection != 0)
+			Debug.Log("Roll " + torqueDirection);
 		// TODO
 	}
 }
--- Assets/Scripts/GamePlay/PlayerCommander.cs
+++ Assets/Scripts/GamePlay/PlayerCommander.cs
@@ -2,7 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerCommander : MonoBehaviour
+/// <summary> This script runs earlier in the Script Execution Order (see Project Settings). </summary>
+[RequireComponent(typeof(Player))]
+public abstract class PlayerCommander : MonoBehaviour
 {
-		
+	private Player myPlayer;
+	/// <summary> Maps a mouse button index to the index of the arm the button is currently controlling, or to -1 if the button isn't controlling an arm. </summary>
+	private int[] mouseButtonToControlledArmIndex = { -1, -1 };
+	private float torqueDirection = 0;
+
+	private void Start()
+	{
+		myPlayer = GetComponent<Player>();
+	}
+
+	private void Update()
+	{
+		// Update the arm controlled by each mouse button.
+		for(int mouseButton=0; mouseButton<mouseButtonToControlledArmIndex.Length; mouseButton++)
+		{
+			int controlledArmIndex = mouseButtonToControlledArmIndex[mouseButton];
+			if (controlledArmIndex == -1) // If the button isn't controlling an arm...
+			{
+				// Can extend and start controlling an arm:
+				if(Input.GetMouseButtonDown(mouseButton))
+				{
+					Vector3 worldMousePosition = WorldMousePosition();
+					int newControlledArmIndex = FindBestArmToTarget(worldMousePosition);
+					myPlayer.ExtendArm(newControlledArmIndex, worldMousePosition);
+					mouseButtonToControlledArmIndex[mouseButton] = newControlledArmIndex;
+				}
+			} else // If the button IS controlling an arm...
+			{
+				// Can detach and stop controlling the current arm:
+				if( ! Input.GetMouseButton(mouseButton))
+				{
+					myPlayer.DetachArm(controlledArmIndex);
+					mouseButtonToControlledArmIndex[mouseButton] = -1;
+				}
+			}
+		}
+
+		// Try to punch with any arms that should punch.
+		for(int armIndex=0; armIndex<Player.armCount; armIndex++)
+		{
+			if(ShouldPunchWithArm(armIndex))
+				myPlayer.PunchWithArm(armIndex);
+		}
+
+		// Determine the torque direction.
+		torqueDirection = GetTorqueDirection();
+	}
+
+	private void FixedUpdate()
+	{
+		// Apply any torque.
+		myPlayer.ApplyRollTorque(torqueDirection);
+	}
+
+	/// <returns> The index of the arm best suited to shooting toward the target world-space position. </returns>
+	public int FindBestArmToTarget(Vector3 targetPosition)
+	{
+		return 0; // TODO
+	}
+
+	public Vector3 WorldMousePosition()
+	{
+		Vector3 result = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+		result.z = 0;
+		return result;
+	}
+
+	public abstract bool ShouldPunchWithArm(int armIndex);
+	public abstract float GetTorqueDirection();
 }
