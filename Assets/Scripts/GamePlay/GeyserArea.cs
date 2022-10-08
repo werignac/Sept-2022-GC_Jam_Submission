ce024f9b7091889306b60f39bd7b3872c3ef9776
werignac <48932607+werignac@users.noreply.github.com>
2022-10-07 22:38:29 -0600

    Geyser Sinusoid
    At the top of the geyser a sinusoid takes effect, bobbing objects up and down.

--- Assets/Scripts/GamePlay/GeyserArea.cs
+++ Assets/Scripts/GamePlay/GeyserArea.cs
@@ -4,19 +4,21 @@ using UnityEngine;
 
 public class GeyserArea : MonoBehaviour
 {
-    [SerializeField]
-    private Vector2 force;
+	[SerializeField]
+	private float forceMagnitude = 20f;
+	[SerializeField]
+	private float timeToFullForce = 1f;
+	[SerializeField]
+	private float sinusoidBottom = 25;
+	[SerializeField]
+	private float sinusoidPeriod = 1f;
 
-    private List<GameObject> pushedObjects;
 
-    void Start()
-    {
-        pushedObjects = new List<GameObject>();
-    }
+	private Dictionary<GameObject, float> pushedObjects = new Dictionary<GameObject, float>();
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        pushedObjects.Add(collision.gameObject);
+        pushedObjects.Add(collision.gameObject, 0);
     }
 
     private void OnTriggerExit2D(Collider2D collision)
@@ -26,17 +28,55 @@ public class GeyserArea : MonoBehaviour
 
     void FixedUpdate()
     {
-        for(int i = 0; i < pushedObjects.Count; ++i)
+		GameObject[] keyCopy = new GameObject[pushedObjects.Count];
+		pushedObjects.Keys.CopyTo(keyCopy, 0);
+
+		float relativeSinusoidCap = GetRelativeHeight(sinusoidBottom);
+
+		foreach (GameObject entry in keyCopy)
         {
-            Rigidbody2D body = pushedObjects[i].GetComponent<Rigidbody2D>();
+            Rigidbody2D body = entry.GetComponent<Rigidbody2D>();
+			
             if(body == null)
             {
-                body = pushedObjects[i].GetComponentInParent<Rigidbody2D>();
+                body = entry.GetComponentInParent<Rigidbody2D>();
             }
             if(body != null)
             {
-                body.AddForce(force);
+				float relativeHeight = GetRelativeHeight(body.transform.position);
+
+				if (relativeHeight > relativeSinusoidCap)
+				{
+					pushedObjects[entry] = (pushedObjects[entry] + Time.fixedDeltaTime) % sinusoidPeriod;
+
+					float sinusoidMultiplier = Mathf.Cos(pushedObjects[entry] * 2 * Mathf.PI / sinusoidPeriod + Mathf.PI) / 2 + 0.5f;
+
+					body.AddForce(transform.up * (forceMagnitude * sinusoidMultiplier));
+				}
+				else
+				{
+					pushedObjects[entry] += 0f;
+					body.AddForce(transform.up * forceMagnitude);
+				}
             }
         }
     }
+
+	private float GetRelativeHeight(Vector3 worldPoint)
+	{
+		Vector3 localPoint = transform.InverseTransformPoint(worldPoint);
+		return GetRelativeHeight(localPoint.y);
+	}
+
+	private float GetRelativeHeight(float localHeight)
+	{
+		BoxCollider2D b_collider = GetComponent<BoxCollider2D>();
+
+		float minHeight = b_collider.offset.y - b_collider.size.y / 2;
+		float maxHeight = b_collider.offset.y + b_collider.size.y / 2;
+
+
+		float alpha = Mathf.Clamp01((localHeight - minHeight) / (maxHeight - minHeight));
+		return alpha;
+	}
 }
